1bdeea48301606e3d3d6f8055da76a2f8deb21f6
Robertuybrush Threepwood <Robertuybrush Threepwood>
2017-06-16 19:11:33 +0200

    lesson18

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -4,4 +4,13 @@
     <h1>{{ $post->title }}</h1>
     <p>{{ $post->user->name  }}</p>
     <p>{{ $post->content }}</p>
+
+    <h4>Comentarios</h4>
+
+    {!! Form::open(['route' => ['comments.store',$post], 'method' => 'POST']) !!}
+        {!! Field::textarea('comment') !!}
+        <button type="submit">
+            Publicar comentario
+        </button>
+    {!! Form::close() !!}
 @endsection
